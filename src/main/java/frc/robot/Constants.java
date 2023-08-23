9bb1c688a6df9900c64a27a6fbbcf3f32a556bb4
Madelena-f <78669731+Madelena-f@users.noreply.github.com>
2023-08-23 16:00:54 -0500

    slew rate didn;t change yet if diver complain lower them

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,8 +31,8 @@ public final class Constants {
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     public static final double kDirectionSlewRate = 1.2; // radians per second
-    public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
-    public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
+    public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%) //lower by .2
+    public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%) //lower by .2
 
     // Chassis configuration
     public static final double kTrackWidth = Units.inchesToMeters(19.5);
