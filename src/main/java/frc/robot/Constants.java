92f4b9d9815d8208d71eb4fd9bf5bd36baaf871a
Madelena-f <78669731+Madelena-f@users.noreply.github.com>
2023-09-07 17:24:55 -0500

    tweaking some rates

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,9 +30,9 @@ public final class Constants {
     public static final double kMaxSpeedMetersPerSecond = 4.8;
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
-    public static final double kDirectionSlewRate = 1.2; // radians per second
-    public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%) //lower by .2
-    public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%) //lower by .2
+    public static final double kDirectionSlewRate = 2.6; // radians per second // 1.2 
+    public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%) 
+    public static final double kRotationalSlewRate = 1.8; // percent per second (1 = 100%) //2
 
     // Chassis configuration
     public static final double kTrackWidth = Units.inchesToMeters(19.5);
