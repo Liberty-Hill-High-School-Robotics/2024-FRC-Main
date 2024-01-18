d68ada4ac4d1d4ff7aa0c98495f2c9db0dff11e7
LeonardoHazembuller <75856125+LeonardoHazembuller@users.noreply.github.com>
2024-01-18 16:07:42 -0600

    Added required subsystems as of 1/16/24

---
+++ src/main/java/frc/robot/subsystems/Bar.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+//imports here
+
+
+public class Bar extends SubsystemBase {
+
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run when in simulation
+
+    }
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+}
