8904236b95cf893fff1a9d3eb9aea50f41bb61bb
LeonardoHazembuller <75856125+LeonardoHazembuller@users.noreply.github.com>
2024-02-06 19:58:34 -0600

    FixJonathan
    I think I fixed the problems Jonathan had

--- src/main/java/frc/robot/commands/IntakeCommands/AutoIntake.java
+++ src/main/java/frc/robot/commands/IntakeCommands/AutoIntake.java
@@ -2,19 +2,25 @@ package frc.robot.commands.IntakeCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Storage;
 
 /**
  * A simple command that grabs a hatch with the {@link HatchSubsystem}. Written explicitly for
  * pedagogical purposes. Actual code should inline a command this simple with {@link
  * edu.wpi.first.wpilibj2.command.InstantCommand}.
  */
-public class Intake extends Command {
+public class AutoIntake extends Command {
   // The subsystem the command runs on
-  private final Intake m_intake;
+  private Intake m_intake;
+  private Storage m_storage;
 
-  public Intake(Intake subsystem) {
+  public void Intake(Intake subsystem) {
     m_intake = subsystem;
     addRequirements(m_intake);
+  }
+  public void Storage(Storage subsystem) {
+    m_storage = subsystem;
+    addRequirements(m_storage);
   }
 
   @Override
@@ -24,7 +30,10 @@ public class Intake extends Command {
 
   @Override
   public void execute(){
-    
+    m_intake.intakePivotDown();
+    m_intake.intakeRollerFeed();
+    m_intake.intakePivotDown();
+    m_storage.storageRollerFeed();
   }
 
   @Override
