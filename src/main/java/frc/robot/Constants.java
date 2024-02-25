fdb41e17326cde24c27af54ebee91b9e43cf7579
Xactlysharp <97402857+Xactlysharp@users.noreply.github.com>
2024-02-25 16:03:14 -0600

    equation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -55,9 +55,9 @@ public final class Constants {
 
     public static final double Slope = 0.05; //angle subtracted with each foot the robot is away from the sub
 
-    public static final double pCalcC = 1680;
+    public static final double pCalcC = 1770;
     public static final double pCalucP = -0.95;
-    public static final double pCaluK = 0.5;
+    public static final double pCaluK = -1.7;
 
   }
   public static final class BarConstants{
