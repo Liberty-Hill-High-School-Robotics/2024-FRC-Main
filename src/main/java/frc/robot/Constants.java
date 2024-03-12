722265bb9822392f18c63f34e78c43b1ff5f12e0
Jtwerp <114360534+Jtwerp@users.noreply.github.com>
2024-03-12 11:17:19 -0500

    Boost Mode
    Changes Max speed to 5.5

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -120,7 +120,7 @@ public final class Constants {
     
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 4.8; //5.7 max mechanical
+    public static double kMaxSpeedMetersPerSecond = 4.8; //5.7 max mechanical
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     public static final double kDirectionSlewRate = 1.2; // radians per second // 1.2 
