b61b541799bebe83ae0b23c7cbdc7c03ea5d1bad
Xactlysharp <97402857+Xactlysharp@users.noreply.github.com>
2024-03-19 08:43:53 -0500

    Remove unused imports

--- src/main/java/frc/robot/commands/SemiAutonomousCommands/AimSub2.java
+++ src/main/java/frc/robot/commands/SemiAutonomousCommands/AimSub2.java
@@ -2,11 +2,9 @@ package frc.robot.commands.SemiAutonomousCommands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.DriveAutonCommands.AimWhileMoving;
 import frc.robot.commands.PivotCommmands.Pivot.AngleSub;
 import frc.robot.commands.ShooterCommands.RevShooterSub;
 //import frc.robot.subsystems.Bar;
-import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Pivot;
 import frc.robot.subsystems.Shooter;
 
