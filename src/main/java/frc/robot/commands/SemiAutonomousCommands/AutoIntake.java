2d391f1f92e1419fc0f8a76624a5fe4b6429d8e4
Xactlysharp <97402857+Xactlysharp@users.noreply.github.com>
2024-03-30 10:30:55 -0500

    Cleanup

--- src/main/java/frc/robot/commands/SemiAutonomousCommands/AutoIntake.java
+++ src/main/java/frc/robot/commands/SemiAutonomousCommands/AutoIntake.java
@@ -3,7 +3,6 @@ package frc.robot.commands.SemiAutonomousCommands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.IntakeCommands.IntakeTogether;
-import frc.robot.commands.LEDCommands.Animations.CandleLarson;
 import frc.robot.commands.LEDCommands.Colors.CandleDimPurple;
 import frc.robot.commands.LEDCommands.Colors.CandleGreen;
 import frc.robot.commands.PivotCommmands.Pivot.AngleAndFeed;
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,41 +1,16 @@
 package frc.robot.subsystems;
 
-
-//unused imports
-
-/*
-import java.util.List;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-*/
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
-//import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import frc.robot.RobotContainer;
 import static java.lang.Math.*;
-
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-/*
-import frc.robot.Constants.AutoConstants;
-import frc.robot.Constants.DriveConstants;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj2.command.Command;
-*/
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
-
 public class Limelight extends SubsystemBase {
-    public static int increment;
+
     public static boolean canSee;
     static NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
 
@@ -60,7 +35,6 @@ public class Limelight extends SubsystemBase {
         SmartDashboard.putNumber("LimelightY", y);
         SmartDashboard.putNumber("LimelightArea", area);
         SmartDashboard.putNumber("GetDistance", getDistance());
-        SmartDashboard.putNumber("increment", increment);
         SmartDashboard.putBoolean("cansee?", canSee);
 
         
@@ -84,9 +58,8 @@ public class Limelight extends SubsystemBase {
     //https://docs.wpilib.org/en/latest/docs/software/vision-processing/introduction/identifying-and-processing-the-targets.html#distance
     //uses this equation ^
     //distance = (targetheight - cameraheight) / tan(cameraangle + Ty)
-    var y = table.getEntry("ty");
-    double distance = (ShooterConstants.ApTagHeight - ShooterConstants.CamHeight) / Math.tan((ShooterConstants.CamAngle + (y.getDouble(0))) * (Math.PI/180));
-    increment++;
-    return distance;
+        var y = table.getEntry("ty");
+        double distance = (ShooterConstants.ApTagHeight - ShooterConstants.CamHeight) / Math.tan((ShooterConstants.CamAngle + (y.getDouble(0))) * (Math.PI/180));
+        return distance;
    }
 }
